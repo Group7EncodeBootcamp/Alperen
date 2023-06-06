f88a8b8dcf8066b4f6836d38673303d9f801147a
alpertncc <alperen.tunc.02@gmail.com>
2023-06-06 17:38:11 +0300

    Started to Homework 7

--- homeworks/SolanaBootcamp/homeworks_rust/homeworks/homework7/modules/modules1.rs
+++ homeworks/SolanaBootcamp/homeworks_rust/homeworks/homework7/modules/modules1.rs
@@ -1,7 +1,7 @@
 // modules1.rs
 // Make me compile! Execute `rustlings hint modules1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
@@ -9,8 +9,9 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
-        get_secret_recipe();
+    pub fn make_sausage() {
+        let secret_recipe = get_secret_recipe();
+        secret_recipe;
         println!("sausage!");
     }
 }
